f013c071593b1beb53c63210324544526cf72ddd
Joseph Muturi <josephmkaranja07@gmail.com>
2023-10-06 18:59:06 +0300

    new task

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+/**
+ * main - print combination of numbers
+ *
+ * Return: Always 0 (Success)
+ */
+/* betty style doc for function main goes there */
+int main(void)
+{
+	/* your code goes there */
+	int n = '0';
+	int k = '1';
+
+	while (n <= '8')
+	{
+		while (k <= '9')
+		{
+			if (k > n)
+			{
+				putchar(n);
+				putchar(k);
+				if (n != '8' || k != '9')
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			k++;
+		}
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
