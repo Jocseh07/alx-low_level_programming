d76d724f0c257bb02bb29c74f6d9c7968ce98ebc
Joseph Muturi <josephmkaranja07@gmail.com>
2023-10-16 22:07:51 +0300

    new task

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * puts2 - prints every other character of a string
+ * starts with first char followed by a new line
+ * @str: string to print
+ */
+
+void puts2(char *str)
+{
+	int i, l;
+
+	l = strlen(str);
+
+	for (i = 0; i < l; i++)
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(s[i]);
+		}
+	}
+
+	_putchar('\n');
+}
