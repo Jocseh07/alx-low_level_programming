5320d48ebe6828e3829bb362b2f39cfb5212bbcb
Joseph Muturi <josephmkaranja07@gmail.com>
2023-10-16 21:38:59 +0300

    new task

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * rev_string - reverses a string
+ * @s: string to be reversed
+ */
+
+void rev_string(char *s)
+{
+	int i, l;
+	char c[];
+
+	i = strlen(s);
+
+	for (l = i - 1; i >= 0; l--)
+	{
+		strcat(i, c);
+	}
+}
