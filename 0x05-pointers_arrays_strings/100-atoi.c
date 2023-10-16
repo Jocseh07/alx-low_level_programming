237a68e1421d6592b2f32cd6a18a589dbfa75df0
Joseph Muturi <josephmkaranja07@gmail.com>
2023-10-16 22:57:35 +0300

    new task

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+
+/**
+ * _atoi - convert a string to an integer
+ * @s: string to be converted
+ * Return: converted string
+ */
+
+int _atoi(char *s)
+{
+	return (atoi(s));
+}
