76441213f055add09af12cc3aa4301850aa2c3db
Joseph Muturi <josephmkaranja07@gmail.com>
2023-10-16 21:20:24 +0300

    new task

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string to get the length
+ * Return: length of the string
+ */
+
+int _strlen(char *s)
+{
+	return (strlen(*s));
+}
