38e138509bc85570cca12122d5b5df1c66cb0fa8
Joseph Muturi <josephmkaranja07@gmail.com>
2023-10-16 22:47:58 +0300

    new task

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,10 +12,10 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < n; i++)
 	{
-		if (i <= 0)
+		if (i == 0)
 		{
 			printf("\n");
-		} else if (i > 0 && i != n - 1)
+		} else if (i != n - 1)
 		{
 			printf("%d, ", a[i]);
 		} else if (i == n - 1)
