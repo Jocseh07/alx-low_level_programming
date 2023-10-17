f5851248cc6a5bc46da11a040951326ce9bbe510
Joseph Muturi <josephmkaranja07@gmail.com>
2023-10-17 21:43:34 +0300

    new task

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,17 +9,5 @@
 
 int _atoi(char *s)
 {
-	int l, i;
-	char c;
-
-	l = 0;
-	i = 0;
-
-	l = strlen(s);
-
-	for (i = 0; i < l; i++)
-	{
-		strcat(c, s[i]);
-	}
-	return (atoi(c));
+	return (atoi(s));
 }
