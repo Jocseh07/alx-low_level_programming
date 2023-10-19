485fee4bf9aeb5229541339249233f6e741ff18f
Joseph Muturi <josephmkaranja07@gmail.com>
2023-10-19 21:39:51 +0300

    new task

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * *infinite_add - adds two numbers
+ * @n1: first number
+ * @n2: second number
+ * @r: buffer that the function will use to store result
+ * @size_r: buffer size
+ * Return: ponter to the result
+ */
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	int l1, l2;
+
+	l1 = strlen(n1);
+	l2 = strlen(n2);
+
+	if (l1 >= size_r || l2 >= size_r)
+	{
+		return (0);
+	}
+	r = n1 + n2;
+
+	return (r);
+}
