9c31f65417aa2d3b242d957eff5374079f43fa09
Joseph Muturi <josephmkaranja07@gmail.com>
2023-10-19 21:03:21 +0300

    new task

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,14 +9,12 @@
 
 char *cap_string(char *s)
 {
-	int l, j, i;
-
-	l = strlen(s);
+	int j, i;
 
 	char sp[13] = {' ', '\t', '\n', ',', ';', '.',
 		'!', '?', '"', '(', ')', '{', '}'};
 
-	for (i = 0; i < l; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
 		{
@@ -25,9 +23,12 @@ char *cap_string(char *s)
 
 		for (j = 0; j < 13; j++)
 		{
-			if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+			if (s[i] == sp[j])
 			{
-				s[i + 1] -= 32;
+				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+				{
+					s[i + 1] -= 32;
+				}
 			}
 		}
 	}
