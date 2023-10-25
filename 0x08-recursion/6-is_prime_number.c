a87f07567262f5bb419478d1614b567da7a61297
Joseph Muturi <josephmkaranja07@gmail.com>
2023-10-25 23:17:52 +0300

    new task

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,5 +1,6 @@
 #include "main.h"
 
+int prime_number(int n, int a);
 /**
  * is_prime_number - return 1 if the integer is a prime no
  * @n: integer to check
@@ -11,8 +12,15 @@ int is_prime_number(int n)
 	if (n <= 1)
 		return (0);
 
-	return (prime_number(n, n-1));
+	return (prime_number(n, n - 1));
 }
+
+/**
+ * prime_number - checks the prime number
+ * @n: the number itself
+ * @a: iterator
+ * Return: 1 if prime 0 otherwise
+ */
 
 int prime_number(int n, int a)
 {
