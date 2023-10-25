47c371465b90303eb34a0b7ca9a17aa26a4c831a
Joseph Muturi <josephmkaranja07@gmail.com>
2023-10-25 22:19:15 +0300

    new task

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: the string
+ * Return: length of the string
+ */
+
+int _strlen_recursion(char *s)
+{
+	int n;
+
+	n = 0;
+
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return (n);
+	}
+
+	if (*s)
+	{
+		n++;
+		_strlen_recursion(s + 1);
+	}
+}
