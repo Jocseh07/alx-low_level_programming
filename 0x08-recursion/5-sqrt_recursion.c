afcc73bdf263f83eab479422720e78dbef30cf6c
Joseph Muturi <josephmkaranja07@gmail.com>
2023-10-25 23:14:58 +0300

    new task

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,12 +1,26 @@
 #include "main.h"
 
+int _sqrt_recursions(int n, int a);
 /**
  * _sqrt_recursion - returns the natural square root of a number
  * @n: the number
  * Return: -1 if does not have natural square root
  */
 
-
+int _sqrt_recursion(int n)
+{
+	if (n < 1)
+	{
+		return (-1);
+	}
+	return (_sqrt_recursions(n, 0));
+}
+/**
+ * _sqrt_recursions - get squareroot of a number
+ * @n: number
+ * @a: iterative
+ * Return: squareroot or -1
+ */
 int _sqrt_recursions(int n, int a)
 {
 	if (a * a > n)
@@ -15,13 +29,5 @@ int _sqrt_recursions(int n, int a)
 	if (a * a == n)
 		return (a);
 
-	return (_sqrt_recursions(n, a+1));
-}
-int _sqrt_recursion(int n)
-{
-	if (n < 1)
-	{
-		return (-1);
-	}
-	return ( _sqrt_recursions(n, 0));
+	return (_sqrt_recursions(n, a + 1));
 }
