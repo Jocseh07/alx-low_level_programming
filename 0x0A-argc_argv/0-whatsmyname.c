d5ed80415b19e347a8ee922d734729876ac7c843
Joseph Muturi <josephmkaranja07@gmail.com>
2023-10-27 23:30:22 +0300

    new task

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,10 +5,11 @@
  * main - prints the name of the program
  * @argc: number of arguments
  * @argv: array of arguments
- * Return: 0
+ * Return: Always 0 (Success)
  */
 int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", *argv);
+
 	return (0);
 }
