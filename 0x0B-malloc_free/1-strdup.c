a8bc3365b57b12b099b17e0b3e2e2f0aac061b09
Joseph Muturi <josephmkaranja07@gmail.com>
2023-10-31 00:39:44 +0300

    new task

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <string.h>
+/**
+ * *_strdup - returns a pointer to a newly allocated
+ * space in memory which contains copy of the string given
+ * @str: string given
+ * Return: Pointer to a new string which duplicates or NULL
+ */
+
+char *_strdup(char *str)
+{
+	char *dup;
+	int l;
+
+	l = strlen(str);
+
+	if (str == NULL)
+		return (NULL);
+
+	dup = malloc(sizeof(char) * (l + 1));
+
+	if (dup == NULL)
+		return (NULL);
+
+	for (i = 0; i < l; i++)
+	{
+		dup[i] == str[i];
+	}
+}
