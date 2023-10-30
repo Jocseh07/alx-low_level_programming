add760b9adcc10bc1d7b146a99946bcab4ba5d4c
Joseph Muturi <josephmkaranja07@gmail.com>
2023-10-30 21:15:31 +0300

    new task

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,2 +1,39 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - add positive numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int sum, i;
+
+	sum = 0
+
+	if (argc == 1)
+	{
+		printf("%d\n", sum);
+		return (0);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		if (atoi(argv[i]))
+		{
+			sum += atoi(atoi(argv[i]));
+		} else
+		{
+			printf("Error\n");
+			return (1);
+		}
+
+		printf("%d\n", sum);
+	}
+
+	return (0);
+}
