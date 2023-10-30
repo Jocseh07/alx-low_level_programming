79e1941e9b8f03ecb690f43a0a1dddf6b0d5eded
Joseph Muturi <josephmkaranja07@gmail.com>
2023-10-30 21:27:35 +0300

    new task

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -23,9 +23,9 @@ int main(int argc __attribute__((unused)), char *argv[]__attribute__((unused)))
 
 	for (i = 1; i < argc; i++)
 	{
-		if ((int) *argv[i])
+		if (atoi(argv[i]))
 		{
-			sum += (int) *argv[i];
+			sum += atoi(argv[i]);
 		} else
 		{
 			printf("Error\n");
