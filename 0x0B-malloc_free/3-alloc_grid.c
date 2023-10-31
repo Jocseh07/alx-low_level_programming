1faa1c884ffd718f3c7f39b70b8d3bb4809a0b7f
Joseph Muturi <josephmkaranja07@gmail.com>
2023-10-31 21:24:59 +0300

    new task

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -18,14 +18,14 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	array = malloc(sizeof(**array) * height);
+	array = (int **) malloc(sizeof(int *) * height);
 
 	if (array == NULL)
 		return (NULL);
 
 	for (i = 0; i < height; i++)
 	{
-		array[i] = malloc(sizeof(int) * width);
+		array[i] = (int *) malloc(sizeof(int) * width);
 		if (array[i] == NULL)
 		{
 			free(array);
