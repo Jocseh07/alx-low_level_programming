8fe1a518350ae78e8528dd62b08560ecc92dd3c8
Joseph Muturi <josephmkaranja07@gmail.com>
2023-10-31 21:10:32 +0300

    new task

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -25,8 +25,11 @@ char *str_concat(char *s1, char *s2)
 	if (s3 == NULL)
 		return (NULL);
 
-	strcat(s3, s1);
-	strcat(s3, s2);
+	if (s1)
+		strcat(s3, s1);
+
+	if (s2)
+		strcat(s3, s2);
 
 	return (s3);
 }
