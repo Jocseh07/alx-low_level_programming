6c5c659c26e5ae01dbd5b764c98f7d3e13f24df5
Jocseh <josephmkaranja07@gmail.com>
2023-11-02 21:41:31 +0300

    new task

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -16,14 +16,14 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	char *old;
 	unsigned int a;
 
+	if (new_size == old_size)
+		return (ptr);
+
 	if (new_size == 0 && ptr)
 	{
 		free(ptr);
 		return (NULL);
 	}
-
-	if (new_size == old_size)
-		return (ptr);
 
 	if (!ptr)
 	{
