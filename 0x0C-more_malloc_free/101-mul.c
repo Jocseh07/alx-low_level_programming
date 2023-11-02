8ec3954275c1b1dc59578da8769b1ae75dc96549
Jocseh <josephmkaranja07@gmail.com>
2023-11-02 21:52:20 +0300

    new task

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+
+/**
+ * main - multiplies 2 positive integers
+ * @num1: one integer
+ * @num2: second integer
+ * Return: multiplication
+ */
+
+int main(unsigned long long int a, unsigned long long int b)
+{
+	return (a * b);
+}
