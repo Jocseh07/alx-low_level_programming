742c7b9d63941847e9186d8cb78bb02c12d13f61
Jocseh <josephmkaranja07@gmail.com>
2023-11-06 21:10:06 +0300

    new task

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,21 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - frees dogs
+ * @d: the structure
+ *
+ * Return: Nothing
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d->name != NULL)
+		free(d->name);
+	if (d->owner != NULL)
+		free(d->owner);
+	if (d->age != NULL)
+		free(d->age);
+	if (d != NULL)
+		free(d);
+}
