c0598bd0c1dd2b5acdd98b2b932a742e8d9df830
Jocseh <josephmkaranja07@gmail.com>
2023-11-06 21:13:19 +0300

    new task

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -10,10 +10,7 @@
 
 void free_dog(dog_t *d)
 {
-	if (d->name != NULL)
-		free(d->name);
-	if (d->owner != NULL)
-		free(d->owner);
-	if (d != NULL)
-		free(d);
+	free(d->name);
+	free(d->owner);
+	free(d);
 }
