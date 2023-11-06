1e4e82a303c44c2432d8210386de61ac82a977f1
Jocseh <josephmkaranja07@gmail.com>
2023-11-06 20:22:51 +0300

    new task

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * struct dog - the dog structure
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
