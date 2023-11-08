5ef98f276193b8e887ce790f40f876f02d42f300
Jocseh <josephmkaranja07@gmail.com>
2023-11-08 14:08:17 +0300

    new task

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,9 +1,9 @@
 #include "function_pointers.h"
 
 /**
- * print_name - funciton that prints a name
- * @name: the name to be printed
- * @f: the function pointer
+ * print_name - prints a name
+ * @name: name to be printed
+ * @f: the pointer to the function
  *
  * Return: Nothing
  */
@@ -11,17 +11,4 @@
 void print_name(char *name, void (*f)(char *))
 {
 	f(name);
-}
-
-void f(char *name)
-{
-	int a = 0, b;
-
-	while (name[a] != '\0')
-		a++;
-
-	for (b = 0; b < a; b++)
-		_putchar(name[b]);
-
-	_putchar('\n');
 }
