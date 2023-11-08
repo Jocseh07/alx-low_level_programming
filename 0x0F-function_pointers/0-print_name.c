ec637a593c5a25943d2cf092f64cc53c3103214d
Jocseh <josephmkaranja07@gmail.com>
2023-11-08 14:15:42 +0300

    new task

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stdlib.h>
 
 /**
  * print_name - prints a name
@@ -11,4 +12,5 @@
 void print_name(char *name, void (*f)(char *))
 {
 	f(name);
+	free(f);
 }
