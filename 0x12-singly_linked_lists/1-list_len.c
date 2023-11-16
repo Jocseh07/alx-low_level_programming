f01207611acf7368fa65aa0b7d04530e39bcba2e
Jocseh <josephmkaranja07@gmail.com>
2023-11-16 22:56:36 +0300

    task 0

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * list_len - return no of elements in linked list
+ * @h: pointer to linked list
+ *
+ * Return: no of elements in h
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t n = 0;
+
+	whle (h)
+	{
+		n++;
+		h->next;
+	}
+
+	return (n);
+}
