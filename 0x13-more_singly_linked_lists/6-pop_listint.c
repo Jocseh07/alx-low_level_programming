d1e9cb55400aa065f77ff7b70ee7f555bb705b7d
Joseph Muturi <josephmkaranja07@gmail.com>
2023-11-21 21:49:23 +0300

    new task

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,24 @@
+#include <lists.h>
+
+/**
+* pop_listint - deletes head node of linked list
+* @head: head of linked list
+*
+* Return: 0 or head node's data
+*/
+
+int pop_listint(listint_t **head)
+{
+	listint_t *temp;
+	int number;
+
+	if (!head)
+		return (0);
+
+	num = (*head)->n;
+	temp = (*head)->next;
+	free(*head);
+	*head = temp;
+
+	return (num);
+}
