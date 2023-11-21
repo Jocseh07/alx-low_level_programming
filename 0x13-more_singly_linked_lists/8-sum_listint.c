9b168ddfec8246d59cb406b7d4a31cf768e72272
Joseph Muturi <josephmkaranja07@gmail.com>
2023-11-21 22:05:51 +0300

    new task

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+* sum_listint - return sum of all data (n) of linked list
+* @head: pointer of first node of linked list
+* Return: 0 if list is empty or NULL
+*/
+
+int sum_listint(listint_t *head)
+{
+	listint_t *temp;
+	int sum = 0;
+
+	while (temp)
+	{
+		sum += temp->n;
+		temp = temp->next;
+	}
+
+	return (sum);
+}
