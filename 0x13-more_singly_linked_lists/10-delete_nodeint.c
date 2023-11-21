d3a4cca2879955ee02d7b4856a499324438a223d
Joseph Muturi <josephmkaranja07@gmail.com>
2023-11-21 22:43:53 +0300

    new task

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -18,8 +18,9 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 
 	if (index == 0)
 	{
-		*head = (*head)->next;
-		free(temp);
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
 		return (1);
 	}
 
