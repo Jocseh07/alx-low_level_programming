c6ab7652fcdeba461761b9212b8d0cc4ce2ab98e
Joseph Muturi <josephmkaranja07@gmail.com>
2023-11-28 01:15:39 +0300

    new task

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,8 @@
+#include "main.h"
+
+/**
+ * create_file - function that creates a file
+ * @filename: name of the file to create
+ * @text_content: NULL terminated string to write to file
+ * Return: 1 on success and -1 on failure
+*/
---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+* read_textfile - reads a text file and prints to standard output
+* @filename: pointer to name of file
+* @letters: number of letters to print
+*
+* Return: if NULL 0 else actual no of letters read and printed
+*/
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t o, r, w;
+	char *name;
+
+	if (filename == NULL)
+		return (0);
+
+	name = malloc(sizeof(char) * letters);
+	if (name == NULL)
+		return (0);
+
+	o = open(filename, 'r');
+	r = read(o, name, letters);
+	w = write(1, name, r);
+
+	if (o == -1 || r == -1 || w == -1 || w != r)
+	{
+		free(name);
+		return (0);
+	}
+	free(name);
+	close(o);
+
+	return (w);
+}
