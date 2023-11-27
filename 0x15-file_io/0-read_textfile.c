1d512d04a8c7d819d3b95c6a5f4de4ecd214d05f
Joseph Muturi <josephmkaranja07@gmail.com>
2023-11-28 00:56:28 +0300

    new task

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -20,7 +20,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (name == NULL)
 		return (0);
 
-	o = open(filename, O_RDONLY);
+	o = open(filename, 'r');
 	r = read(o, name, letters);
 	w = write(1, name, r);
 
