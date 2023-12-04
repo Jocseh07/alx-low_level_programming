f4e0176c6f7ca0706ba639205d298725bd1dd465
Joseph Muturi <josephmkaranja07@gmail.com>
2023-12-04 15:44:37 +0300

    13

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -9,14 +9,14 @@
 
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int o, w, l;
+	int o, w, l = 0;
 
 	if (filename == NULL)
 		return (-1);
 
 	if (text_content != NULL)
 	{
-		for (l = 0; text_content[l]; )
+		for (l = 0; text_content[l];)
 			l++;
 	}
 
