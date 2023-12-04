2030a35c272161f4851866e2c77b75490265b607
Joseph Muturi <josephmkaranja07@gmail.com>
2023-12-04 16:31:12 +0300

    some more fixes

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -67,7 +67,7 @@ int main(int argc, char *argv[])
 	r = read(file_from, buffer, 1024);
 	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
-	while (r > 0)
+	do
 	{
 		if (file_from == -1 || r == -1)
 		{
@@ -88,9 +88,9 @@ int main(int argc, char *argv[])
 
 		r = read(file_from, buffer, 1024);
 		file_to = open(argv[2], O_WRONLY | O_APPEND);
-	}
+	} while (r > 0)
 
-	free(buffer);
+		free(buffer);
 	close_file(file_from);
 	close_file(file_to);
 
