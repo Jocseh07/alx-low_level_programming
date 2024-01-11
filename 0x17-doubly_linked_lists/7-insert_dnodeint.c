070e931623ffa035f751481ba86c747050cbd79d
Joseph Muturi <josephmkaranja07@gmail.com>
2024-01-11 14:27:48 +0300

    2

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -22,27 +22,30 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (new);
 	}
 	head2 = *h;
-	while (head2 != NULL && head2->prev != NULL)
-		head2 = head2->prev;
-	while (head2 != NULL)
+	if (head2 != NULL)
 	{
-		if (a == idx)
-			break;
-		a++;
-		head2 = head2->next;
-	}
-	if (head2->next == NULL && head2 != NULL)
-		new = add_dnodeint_end(h, n);
-	else if (head2 != NULL)
-	{
-		new = malloc(sizeof(dlistint_t));
-		if (new == NULL)
-			return (NULL);
-		new->n = n;
-		new->next = head2->next;
-		new->prev = head2;
-		head2->next = new;
-		head2->next->prev = new;
+		while (head2->prev != NULL)
+			head2 = head2->prev;
+		while (head2 != NULL)
+		{
+			if (a == idx)
+				break;
+			a++;
+			head2 = head2->next;
+		}
+		if (head2 != NULL)
+			new = add_dnodeint_end(h, n);
+		else if (head2 != NULL)
+		{
+			new = malloc(sizeof(dlistint_t));
+			if (new == NULL)
+				return (NULL);
+			new->n = n;
+			new->next = head2->next;
+			new->prev = head2;
+			head2->next = new;
+			head2->next->prev = new;
+		}
 	}
 	return (new);
 }
