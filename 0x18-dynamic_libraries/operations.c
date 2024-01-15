e046498a3d478176787fc8cee9fb7514c7fc1c39
Joseph Muturi <josephmkaranja07@gmail.com>
2024-01-15 20:09:18 +0300

    readme

---
+++ 0x18-dynamic_libraries/operations.c
@@ -0,0 +1,59 @@
+/**
+ * add - add two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: result of add
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - sub two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: result of sub
+ */
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - mul two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: result of mul
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - div two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: result of div
+ */
+
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - mod two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: result of mod
+ */
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
