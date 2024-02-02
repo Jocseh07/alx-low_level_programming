bcf97353a1f1747cfa36af40cf8d5b632e31fc0b
Jocseh07 <josephmkaranja07@gmail.com>
2024-02-02 22:46:50 +0300

    Mandatory

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -15,6 +15,8 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 		return (NULL);
 
 	index = key_index((const unsigned char *)key, ht->size);
+	if (index >= ht->size)
+		return (NULL);
 	for (a = index; ht->array[a]; a++)
 	{
 		if (strcmp(ht->array[a]->key, key) == 0)
